4a5167a18b92d2ef1a5edef357d94db21475b473
Steve Choi <stevechoi93@gmail.com>
2019-01-21 20:36:10 -0800

    design outline finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
+
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import ScrollIntoView from "react-scroll-into-view";
+
+const containerStyle = {
+  borderWidth: "3",
+  minHeight: "400px",
+  borderStyle: "solid"
+};
+
+const Container = () => <div style={containerStyle}>image</div>;
+
+const centerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "92vh"
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h1 style={centerStyle}>Steve Choi</h1>
+
+          <i className="fas fa-arrow-down" />
+        </div>
+        <Container className="Container" />
+        <h1>asdfasdf</h1>
+        <Button>
+          <i class="fas fa-at" />
+        </Button>
+        <Button>
+          <i class="fab fa-github" />
+        </Button>
+        <Button>
+          <i class="fas fa-file" />
+        </Button>
+        <h1>Copyright Steve</h1>
       </div>
     );
   }
