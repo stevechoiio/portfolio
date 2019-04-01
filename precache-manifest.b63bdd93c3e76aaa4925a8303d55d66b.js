6495dad9d542f90c40a249eb7ff7f1fefd07914b
Steve Choi <stevechoi93@gmail.com>
2019-04-01 15:10:38 -0700

    Updates

---
+++ precache-manifest.b63bdd93c3e76aaa4925a8303d55d66b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2845ee35eb6f98454d4e",
+    "url": "/portfolio/static/css/main.48bc7238.chunk.css"
+  },
+  {
+    "revision": "2845ee35eb6f98454d4e",
+    "url": "/portfolio/static/js/main.2845ee35.chunk.js"
+  },
+  {
+    "revision": "62c981231e90dea396ba",
+    "url": "/portfolio/static/js/1.62c98123.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "ea7fd51ad55c4724efbe07785283bf69",
+    "url": "/portfolio/index.html"
+  }
+];
